c9801ea138e4081bc6f91a63a1d75d42d809871d
Zaidroid <36863373+Zaidroid@users.noreply.github.com>
2025-03-03 09:56:36 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,26 +9,25 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // Dynamically created colors, will be used with CSS variables
+        // Using RGB variables for dynamic color theming
         primary: {
-          50: 'var(--color-primary-50)',
-          100: 'var(--color-primary-100)',
-          200: 'var(--color-primary-200)',
-          300: 'var(--color-primary-300)',
-          400: 'var(--color-primary-400)',
-          500: 'var(--color-primary-500)',
-          600: 'var(--color-primary-600)',
-          700: 'var(--color-primary-700)',
-          800: 'var(--color-primary-800)',
-          900: 'var(--color-primary-900)',
-          950: 'var(--color-primary-950)',
+          50: 'rgb(var(--color-primary-50))',
+          100: 'rgb(var(--color-primary-100))',
+          200: 'rgb(var(--color-primary-200))',
+          300: 'rgb(var(--color-primary-300))',
+          400: 'rgb(var(--color-primary-400))',
+          500: 'rgb(var(--color-primary-500))',
+          600: 'rgb(var(--color-primary-600))',
+          700: 'rgb(var(--color-primary-700))',
+          800: 'rgb(var(--color-primary-800))',
+          900: 'rgb(var(--color-primary-900))',
+          950: 'rgb(var(--color-primary-950))',
         },
         dark: {
           background: 'rgb(10, 10, 20)',
           card: 'rgb(20, 20, 35)',
           foreground: 'rgb(250, 250, 255)',
           text: 'rgb(200, 200, 220)',
-          primary: 'var(--color-primary-500)',
           muted: 'rgb(40, 40, 60)'
         }
       },
